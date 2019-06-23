2457c840c0ec2f1553d173264525b1a30b3e2301
juanauburn <juanauburn@gmail.com>
2019-06-23 12:17:24 -0500

    Optimized prime number

--- program.cpp
+++ program.cpp
@@ -4,32 +4,31 @@
 #include <dos.h>
 using namespace std;
 
-bool is_prime_number(int number) {
-	if (number > 1) {
-		int limit = number/2;
-		for(int count = 2; count <= limit; count++)
-		{
-			if(number % count == 0) {
-				return false;
-			}
-		}
-		return true;
-	} else {
-		cout<<"The number must be greater than 1"<<endl;
-		return false;
-	}
+bool is_prime_number(int n) {
+	// Casos de esquina
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    
+    if (n % 2 == 0 || n % 3 == 0) 
+        return false; 
+  
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false; 
+  
+    return true;
 }
 
 int main(void) {
 	clock_t start, end; 
 	start = clock();
 	cout << "Start: "<<start<<endl;
-	const unsigned long number_to_check = 2147483647;
-	if (is_prime_number(number_to_check)) {
-		cout << "Number is Prime."<<endl;
-	} else {
-		cout << "Number is not Prime."<<endl;
-	}
+	
+	const int number_to_check = 2147483647;
+	is_prime_number(number_to_check) ? cout<<"True\n" : cout<<"False\n";
+	
 	end = clock();
 	cout << "End: "<<end<<endl;
 	printf("The time was: %i\n", (end - start)); 
