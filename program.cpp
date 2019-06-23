199035d7671e8c4b0da0598f085a278c86746b59
juanauburn <juanauburn@gmail.com>
2019-06-23 13:22:56 -0500

    MultiThreading prime number

--- program.cpp
+++ program.cpp
@@ -2,35 +2,52 @@
 #include <time.h> 
 #include <stdio.h> 
 #include <dos.h>
+#include <thread>
 using namespace std;
 
-bool is_prime_number(int n) {
-	// Casos de esquina
+bool is_prime_number(int n, bool *result, int start, int end) {
     if (n <= 1)
-        return false;
+    	return *result = false;
+    	
     if (n <= 3)
-        return true;
-    
-    if (n % 2 == 0 || n % 3 == 0) 
-        return false; 
+    	return *result = true;
+    	
+    if (start <= 1)
+		start = 2;
   
-    for (int i = 5; i * i <= n; i = i + 6)
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false; 
-  
-    return true;
+    for (int i = start; i <= end; i++)
+        if (n % i == 0 || *result == false)
+        	return *result = false;
 }
 
 int main(void) {
 	clock_t start, end; 
 	start = clock();
-	cout << "Start: "<<start<<endl;
+	//cout << "Start: "<<start<<endl;
 	
 	const int number_to_check = 2147483647;
-	is_prime_number(number_to_check) ? cout<<"True\n" : cout<<"False\n";
+	int number_of_threads = 4;
+	thread myThreads[number_of_threads];
+	bool prime = true;
+	int jump = (number_to_check/2)/number_of_threads;
+	if (jump < 1) {
+		jump = number_to_check/2;
+		number_of_threads = 1;
+	}
+	for(int i=0; i<number_of_threads; i++) {
+		//cout<<"Hilo: "<<i<<endl;
+		int start = i * jump;
+		int end = start + jump;
+		myThreads[i] = thread(is_prime_number, number_to_check, &prime, start, end);
+	}
+	for (int i=0; i<number_of_threads; i++){
+		//cout<<"Join hilo: "<<i<<endl;
+        myThreads[i].join();
+    }
+	prime ? cout<<"True\n" : cout<<"False\n";
 	
 	end = clock();
-	cout << "End: "<<end<<endl;
+	//cout << "End: "<<end<<endl;
 	printf("The time was: %i\n", (end - start)); 
 	return 0;
 }
